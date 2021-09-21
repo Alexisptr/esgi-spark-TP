bfd0e908723cb620aa40941336407b4b9ba93707
Alexis Petrillo <alexis@MacBook-Pro-de-Alexis.local>
2021-09-21 16:35:33 +0200

    TP2

---
+++ TP2.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("TP2") \
+    .master("local[*]") \
+    .getOrCreate()
+
+text = spark.sparkContext.textFile("data/spark-wikipedia.txt")
+
+print(text.count)
