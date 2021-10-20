419b06132ac2dc02106ee9462dd091f638cfbe2c
Alexis Petrillo <alexis@MacBook-Pro-de-Alexis.local>
2021-10-20 18:52:05 +0200

    correction TP2

--- TP2.py
+++ TP2.py
@@ -8,4 +8,18 @@ spark = SparkSession \
 
 text = spark.sparkContext.textFile("data/spark-wikipedia.txt")
 
-print(text.count())
+textRdd = text.flatMap(lambda line: line.split(" ")) \
+    .map(lambda word: (word, 1)) \
+    .reduceByKey(lambda a, b: a + b) \
+    .sortBy(lambda a: a[1], ascending=False) \
+    .coalesce(1)
+
+textRdd.saveAsTextFile("results/wordcount.csv")
+
+print(textRdd.getNumPartitions())
+
+accum = spark.sparkContext.accumulator(0)
+
+text.filter(lambda l: "Spark" in l).foreach(lambda x: accum.add(1))
+
+print(accum.value)
