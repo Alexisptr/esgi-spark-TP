b854dcde4ad31d4cbfee15f3ee42c6b96b2d1c53
Alexis Petrillo <alexis@MacBook-Pro-de-Alexis.local>
2021-12-14 14:52:59 +0100

    TP4

--- TP4-bis.py
+++ TP4-bis.py
@@ -16,3 +16,57 @@ delay_flights_df = spark.read.format("csv") \
     .load(delay_flights_file)
 
 delay_flights_df.createOrReplaceTempView("delay_flights_table")
+
+# 1
+spark.sql("""SELECT *
+FROM delay_flights_table
+WHERE distance > 1000""").show()
+
+# 2
+spark.sql("""SELECT date, delay, origin, destination
+FROM delay_flights_table
+WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD'
+ORDER by delay DESC""").show()
+
+# 3
+spark.sql("""SELECT delay, origin, destination, 
+CASE 
+WHEN delay >= 360 THEN 'Très long retard' 
+WHEN delay >= 120 AND delay < 360 THEN 'Long retard' 
+WHEN delay >= 60 AND delay < 120 THEN 'Retard modéré' 
+WHEN delay > 0 and delay < 60 THEN 'Retard tolérable' 
+WHEN delay = 0 THEN 'Pas de retard' 
+ELSE 'En avance' 
+END AS delay_type 
+FROM delay_flights_table 
+ORDER BY origin, delay DESC""").show()
+
+# 4
+spark.sql("""DROP TABLE IF EXISTS departureDelays""")
+
+spark.sql("""CREATE TABLE departureDelays AS 
+SELECT origin, destination, SUM(delay) AS TotalDelays 
+FROM delay_flights_table 
+WHERE origin IN ('SEA', 'SFO', 'JFK') 
+AND destination IN ('SEA', 'SFO', 'JFK', 'DEN', 'ORD', 'LAX', 'ATL') 
+GROUP BY origin, destination""")
+
+spark.sql("""SELECT * FROM departureDelays""").show()
+
+# 5
+spark.sql("""
+SELECT origin, destination, TotalDelays, rank 
+FROM ( 
+    SELECT origin, destination, TotalDelays, dense_rank() 
+    OVER (PARTITION BY origin ORDER BY TotalDelays DESC) as rank 
+    FROM departureDelays 
+) t 
+WHERE rank <= 3""").show()
+
+# 6
+def route(origin, destination):
+    return origin + "-" + destination
+
+spark.udf.register("route", route, StringType())
+
+spark.sql("SELECT *, route(origin, destination) AS route FROM delay_flights_table").show()
--- TP4.py
+++ TP4.py
@@ -4,6 +4,7 @@ spark = SparkSession \
     .builder \
     .appName("TP4") \
     .master("local[*]") \
+    .enableHiveSupport() \
     .getOrCreate()
 
 mnm_file = "data/mnm.csv"
@@ -14,3 +15,16 @@ mnm_df = spark.read.format("csv") \
     .load(mnm_file)
 
 mnm_df.createOrReplaceTempView("mnm_table")
+
+# 1
+spark.sql("""SELECT State, SUM(Count) AS Total
+FROM mnm_table
+GROUP BY State
+ORDER BY Total DESC""").show(n=10, truncate=False)
+
+# 2
+spark.sql("""SELECT State, Color, SUM(Count) As Total
+FROM mnm_table
+WHERE State == "CA"
+GROUP BY State, Color
+ORDER BY Total DESC""").show(n=10, truncate=False)
